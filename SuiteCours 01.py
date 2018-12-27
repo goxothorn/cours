364bcef7cd0f03f05c13edb199abee92dc9f8128
goxothorn <43968281+goxothorn@users.noreply.github.com>
2018-12-27 15:24:38 +0100

    maj

--- SuiteCours 01.py
+++ SuiteCours 01.py
@@ -5,3 +5,28 @@ def test ():
     return a
 a= test()
 print (a)
+
+print ("test")
+
+class DicoOrdonne:
+
+    def __init__(self, base = {}, **data):
+        self.key = []
+        self.value = []
+
+        for cle in base:
+            self.key.append(cle)
+
+        for values in base.values():
+            self.value.append(values)
+
+
+
+
+test= {"pomme" : 5, "poire" : 10}
+test
+for cle in test:
+    print (cle)
+
+for valeur in test.values():
+    print (valeur)
